04e659ea8af90101dae626a37fac3db2ede28b54
Ridhima Bansal <bansal.ridhima30@gmail.com>
2020-09-25 18:21:41 +0530

    spring boot pt1

---
+++ MessageProducer/src/main/java/com/citi/MessageProducerController.java
@@ -0,0 +1,22 @@
+package com.citi;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageProducerController {
+
+	@Autowired
+	MessageSenderService messageSenderService;
+	
+	@GetMapping(value="/producer")
+	public String sendMessage(@RequestParam("message")String message) {
+		messageSenderService.sendTextMessage(message);
+		return "Your message sent successfully!!";
+	}
+	
+}
